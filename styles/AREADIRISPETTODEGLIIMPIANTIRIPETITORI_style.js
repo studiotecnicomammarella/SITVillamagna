d216983707ca7819b585d46e9f11d9cd6b6e73b7
studiotecnicomammarella <studiotecnicomammarella@gmail.com>
2026-02-20 03:45:06 +0100

    Add files via upload

---
+++ styles/AREADIRISPETTODEGLIIMPIANTIRIPETITORI_style.js
@@ -0,0 +1,29 @@
+var clusterSize = 0;
+    
+var style_AREADIRISPETTODEGLIIMPIANTIRIPETITORI = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var labelText = "";
+    var labelFont = "13.0px \'MS Shell Dlg 2\', sans-serif";
+    var labelFill = 'rgba(50,50,50,1.0)';
+    var bufferColor = 'rgba(250,250,250,1.0)';
+    var bufferWidth = 3.0;
+    var textAlign = "left";
+    var textPlacement = 'point';
+    var offsetX = 8;
+    var offsetY = 3; 
+    var value = feature.get("") || "";
+    if (feature.get("Text") !== null) {
+        labelText = String(feature.get("Text"));
+    }
+        var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(116,202,231,0.4980392156862745)'}),
+            text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, textPlacement, bufferColor,
+                              bufferWidth)
+            })];
+    return style;
+};
+    
---
+++ styles/AREEDICONCENTRAZIONEDEGLISTANDARD_style.js
@@ -0,0 +1,29 @@
+var clusterSize = 0;
+    
+var style_AREEDICONCENTRAZIONEDEGLISTANDARD = function(feature, resolution){
+    var context = {
+        feature: feature,
+        variables: {}
+    };
+    var labelText = "";
+    var labelFont = "10px, sans-serif";
+    var labelFill = '';
+    var bufferColor = '';
+    var bufferWidth = 0;
+    var textAlign = "left";
+    var textPlacement = 'point';
+    var offsetX = 8;
+    var offsetY = 3; 
+    var value = feature.get("") || "";
+    if ("" !== null) {
+        labelText = String("");
+    }
+        var style = [ new ol.style.Style({
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(255,148,0,0.4980392156862745)'}),
+            text: createTextStyle(feature, resolution, labelText, labelFont,
+                              labelFill, textPlacement, bufferColor,
+                              bufferWidth)
+            })];
+    return style;
+};
+    
